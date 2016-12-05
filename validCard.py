e2c0288d43d232c5d95ce4f62c39cab4059d54b6
joeb3rry <joeberry277@gmail.com>
2016-12-05 15:23:55 -0500

    Create validCard.py

---
+++ validCard.py
@@ -0,0 +1,27 @@
+from database_nocsv import Database
+from database_nocsv import credentials
+from database_nocsv import paymentmethoddb
+
+
+#Validates credit card length (no str)
+            
+def validCard (cardno):
+    
+    if len(str(cardno)) != 16:
+        print ("Please enter 16 digit card number")
+        Database.addPaymentMethod (self, username, paymentmethod)
+        
+    else:
+        i = 0
+        while i < len(str(cardno)):
+            c = (str(cardno))
+            if c in range (0,47) or c in range (58,255):
+                print ("Invalid value")
+                break
+            else:
+                i = i + 1
+
+    
+    
+    print ("Successfully added credit card")
+
