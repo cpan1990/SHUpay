df8cf2414dec995ae410464806bf3c165095988a
Rich <red_guard19@yahoo.com>
2016-12-05 19:25:21 -0500

    Add files via upload

---
+++ unittest.py
@@ -0,0 +1,12 @@
+import unittest
+import SHUpayMenuA.py
+
+from database_nocsv import Database
+from database_nocsv import balancedb
+from accounts import Accounts
+from database_nocsv import credentials
+
+class MenuUnitTest (unittest.TestCase):
+    def test_Menu(self):
+        Database.balancedb (self, username, bank_balance)
+
